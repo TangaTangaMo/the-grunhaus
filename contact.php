edc6c4c5ec587b5f4de6d57f3b932d67556485d4
TangaTangaMo <itsamoaway@gmail.com>
2025-05-08 17:09:31 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+// Get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $subject = test_input($_POST["subject"]);
+  $message = test_input($_POST["message"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
